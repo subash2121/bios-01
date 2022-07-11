05d8574a3278039100df031e29e26ab2f9c3a2db
Subash A <subash@SUI-C0044-X0.local>
2022-07-11 22:23:41 +0530

    Add distance computation in point entity

--- lib/geometry/point.rb
+++ lib/geometry/point.rb
@@ -1,8 +1,21 @@
 module Geometry
   class Point
+    attr_reader :x
+    attr_reader :y
+
     def initialize(x, y)
       @x = x
       @y = y
+    end
+
+    def distance_from(dest_point)
+      if dest_point.is_a?(Geometry::Point)
+        x_intercept = (x - dest_point.x).abs
+        y_intercept = (y - dest_point.y).abs
+        return Math.sqrt(x_intercept ** 2 + y_intercept ** 2).round(2)
+      else
+        raise("Point entity is expected")
+      end
     end
   end
 end
--- spec/geometry/point_spec.rb
+++ spec/geometry/point_spec.rb
@@ -1,7 +1,51 @@
 RSpec.describe Geometry::Point do
-  context "when testing Point class" do
+  context "Point entity" do
     it "should be initialised with two coordinates x,y" do
       expect { Geometry::Point.new(1, 2) }.not_to raise_error
+    end
+  end
+
+  context "length for horizontal lines" do
+    it "should return 1 for 1,2 and 2,2" do
+      point_one = Geometry::Point.new(1, 2)
+      point_two = Geometry::Point.new(2, 2)
+      expect(point_one.distance_from(point_two)).to be 1.0
+    end
+
+    it "should return 2 for 0,0 and 2,0" do
+      point_one = Geometry::Point.new(0, 0)
+      point_two = Geometry::Point.new(2, 0)
+      expect(point_one.distance_from(point_two)).to be 2.0
+    end
+  end
+
+  context "length for vertical lines" do
+    it "should return 3 for 1,1 and 1,4" do
+      point_one = Geometry::Point.new(1, 1)
+      point_two = Geometry::Point.new(1, 4)
+      expect(point_one.distance_from(point_two)).to be 3.0
+    end
+
+    it "should return 4 for 0,0 and 0,4" do
+      point_one = Geometry::Point.new(0, 0)
+      point_two = Geometry::Point.new(0, 4)
+      expect(point_one.distance_from(point_two)).to be 4.0
+    end
+  end
+
+  context "length for square diagonal lines" do
+    it "should return 2.83 for 0,0 and 2,2" do
+      point_one = Geometry::Point.new(0, 0)
+      point_two = Geometry::Point.new(2, 2)
+      expect(point_one.distance_from(point_two)).to eq 2.83
+    end
+  end
+
+  context "length for rectangular diagonal lines" do
+    it "should return 5 for 0,1 and 3,5" do
+      point_one = Geometry::Point.new(0, 1)
+      point_two = Geometry::Point.new(3, 5)
+      expect(point_one.distance_from(point_two)).to be 5.0
     end
   end
 end
